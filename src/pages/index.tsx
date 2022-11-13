60806a80373dd18f50769c18fb44729af120ce05
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-11-13 11:32:00 -0300

    finishing challenge, with page/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ interface Image {
 }
 
 interface GetImagesResponse {
-  atfer: string;
+  after: string;
   data: Image[];
 }
 
@@ -39,14 +39,13 @@ export default function Home(): JSX.Element {
     fetchNextPage,
     hasNextPage,
   } = useInfiniteQuery('images', fetchImages, {
-    getNextPageParam: lastPage => lastPage?.atfer || null,
+    getNextPageParam: lastPage => lastPage?.after || null,
   });
 
   const formattedData = useMemo(() => {
     const formatted = data?.pages.flatMap(imageData => {
       return imageData.data.flat();
     });
-
     return formatted;
   }, [data]);
 
@@ -54,7 +53,7 @@ export default function Home(): JSX.Element {
     return <Loading />;
   }
 
-  if (isLoading && isError) {
+  if (!isLoading && isError) {
     return <Error />;
   }
 
@@ -62,9 +61,14 @@ export default function Home(): JSX.Element {
     <>
       <Header />
 
-      <Box maxW={1120} px={20} mx="auto" my={20}>
+      <Box maxW={1120} px={[10, 15, 20]} mx="auto" my={[10, 15, 20]}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
